fc3b36ce4a17ed8f0a695ab3d666f327ee50aa5d
anvarturakulov <anvarsam@mail.ru>
2020-05-02 15:51:19 +0500

    add new-items

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    var mixer = mixitup('.products__inner-box');
+    
 
     $(".rate-star").rateYo({
         rating: 5,
@@ -13,5 +13,28 @@ $(function(){
       dots: true,
       arrows:false
     });
+
+    $(".js-range-slider").ionRangeSlider({
+      type: "double",
+      min: 0,
+      max: 1000,
+      from: 0,
+      to: 600,
+      prefix: "$"
+  });
+
+  $(".icon-th-list").on('click',function(){
+    $('.product__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon--th-large').removeClass('active');
+  });
+
+  $(".icon--th-large").on('click',function(){
+    $('.product__item').removeClass('list');
+    $('.icon-th-list').removeClass('active');
+    $('.icon--th-large').addClass('active');
+  });
+
+  var mixer = mixitup('.products__inner-box');
 
 });
