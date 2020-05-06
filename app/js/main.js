21ad3af9761735fc44b4a6c6309b05481e8d4846
anvarturakulov <anvarsam@mail.ru>
2020-05-06 16:05:47 +0500

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
+// $(function scrollTo(element){
+//   window.scroll({
+//     left: 0,
+//     top: element.offsetTop,
+//     behavior: 'smooth'
+//   })
+// });
+
+// var btn__scroll = document.querySelector('.btn-scroll');
+
+// btn__scroll.addEventListener('click', () => {
+//   console.log('privet')
+//   //scrollTo(footer);
+// });
+
+
 $(function(){
-    
 
     $(".rate-star").rateYo({
         rating: 5,
@@ -34,6 +49,18 @@ $(function(){
     $('.icon-th-list').removeClass('active');
     $('.icon--th-large').addClass('active');
   });
+
+
+  $('.header__btn-menu').on('click',function(){
+    $('.header__box').toggleClass('active');
+  });
+
+  $('.menu__btn').on('click',function(){
+    $('.menu__list').slideToggle();
+  });
+
+  
+
 
   var mixer = mixitup('.products__inner-box');
 
